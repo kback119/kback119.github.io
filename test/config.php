0b0ee3aec4fda75f700d934e031e3f58127c547e
kback119 <35928891+kback119@users.noreply.github.com>
2018-03-22 22:29:48 -0400

    Add files via upload

---
+++ test/config.php
@@ -0,0 +1,15 @@
+<?php
+ 
+
+ $dbhost = "localhost";
+ $dbuser = "root";
+ $dbpass = "root";
+ $dbname = "kb_contact";
+ 
+ // Create connection
+ $mysqli = mysqli_connect($dbhost, $dbuser, $dbpass, $dbname);
+ 
+}
+ 
+?>
+
